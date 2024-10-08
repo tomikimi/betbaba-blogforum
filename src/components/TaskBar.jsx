36e02729ebe6f5323663cf867c21a9f7dc614686
Tomikimi <tomikimiogoli@yahoo.com>
2024-10-08 11:55:00 +0100

    changed css file for Taskbar page

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -1,5 +1,5 @@
-import { FaNewspaper, FaCheck } from "react-icons/fa6";
-import styles from "./taskBar.module.css";
+import { FaNewspaper } from "react-icons/fa6";
+import styles from "./Taskbar.module.css";
 import { NavLink } from "react-router-dom";
 function TaskBar({ path }) {
   return (
